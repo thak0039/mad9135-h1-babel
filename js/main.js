86eb8312ec52806d94bc835b92ca72fc0cffac28
thak0039 <thak0039@algonquinlive.com>
2019-09-19 18:00:40 -0400

    master

--- js/main.js
+++ js/main.js
@@ -1,45 +1,62 @@
 'use strict';
 
+// async function with await and fetch
 var getplaceholder = function () {
-    var _ref = _asyncToGenerator( /*#__PURE__*/regeneratorRuntime.mark(function _callee() {
-        var p, data, data2;
-        return regeneratorRuntime.wrap(function _callee$(_context) {
-            while (1) {
-                switch (_context.prev = _context.next) {
-                    case 0:
-                        p = document.getElementById('userid').value;
+  var _ref = _asyncToGenerator( /*#__PURE__*/regeneratorRuntime.mark(function _callee() {
+    var p, data, data2, arr, personId, name, ans, myObject;
+    return regeneratorRuntime.wrap(function _callee$(_context) {
+      while (1) {
+        switch (_context.prev = _context.next) {
+          case 0:
+            p = document.getElementById('userid').value;
+            _context.next = 3;
+            return fetch('https://jsonplaceholder.typicode.com/users');
 
-                        console.log(p);
+          case 3:
+            data = _context.sent;
+            _context.next = 6;
+            return data.json();
 
-                        _context.next = 4;
-                        return fetch('https://jsonplaceholder.typicode.com/users');
+          case 6:
+            data2 = _context.sent;
 
-                    case 4:
-                        data = _context.sent;
-                        _context.next = 7;
-                        return data.json();
+            document.getElementById('vel').innerHTML = '\n             <ul>\n            <li>id:' + data2[0].id + '</li>\n            <li>name:' + data2[0].name + '</li>\n            <li>email:' + data2[0].email + '</li>\n        </ul>\n            ';
+            // object assignment destructuring
+            arr = [data2[0].id, data2[0].name];
+            personId = arr[0], name = arr[1];
+            ans = " ID = " + personId + " Name = " + name;
 
-                    case 7:
-                        data2 = _context.sent;
+            document.getElementById("desstruc").innerHTML = ans;
+            //es6 method shorthand in objects
+            myObject = {
+              userFirstName: 'Pinki',
+              userLastName: 'Thakor',
+              output: function output() {
+                var printy = document.getElementById('printShortObj');
 
-                        document.getElementById('vel').innerHTML = '\n            <ul>\n            <li>id:' + data2[0].id + '</li>\n            <li>name:' + data2[0].name + '</li>\n            <li>email:' + data2[0].email + '</li>\n        </ul>\n            ';
+                printy.innerHTML = this.userFirstName + ' ' + this.userLastName;
+              }
+            };
 
-                    case 9:
-                    case 'end':
-                        return _context.stop();
-                }
-            }
-        }, _callee, this);
-    }));
 
-    return function getplaceholder() {
-        return _ref.apply(this, arguments);
-    };
+            myObject.output();
+
+          case 14:
+          case 'end':
+            return _context.stop();
+        }
+      }
+    }, _callee, this);
+  }));
+
+  return function getplaceholder() {
+    return _ref.apply(this, arguments);
+  };
 }();
 
 function _asyncToGenerator(fn) { return function () { var gen = fn.apply(this, arguments); return new Promise(function (resolve, reject) { function step(key, arg) { try { var info = gen[key](arg); var value = info.value; } catch (error) { reject(error); return; } if (info.done) { resolve(value); } else { return Promise.resolve(value).then(function (value) { step("next", value); }, function (err) { step("throw", err); }); } } return step("next"); }); }; }
 
 document.addEventListener("DOMContentLoaded", function () {
 
-    document.getElementById('btnuser').addEventListener('click', getplaceholder);
+  document.getElementById('btnuser').addEventListener('click', getplaceholder);
 });
--- src/main.js
+++ src/main.js
@@ -5,21 +5,38 @@ document.addEventListener("DOMContentLoaded", () => {
      document.getElementById('btnuser').addEventListener('click',getplaceholder);    
     
   });
-  
+  // async function with await and fetch
   async function getplaceholder(){
        let p = document.getElementById('userid').value;
-       console.log(p);
-
           let data = await fetch('https://jsonplaceholder.typicode.com/users')
           let data2 = await data.json()
           document.getElementById('vel').innerHTML=`
-            <ul>
+             <ul>
             <li>id:${data2[0].id}</li>
             <li>name:${data2[0].name}</li>
             <li>email:${data2[0].email}</li>
         </ul>
             `
-        
-    
-        
+   // object assignment destructuring
+            let arr = [data2[0].id, data2[0].name]
+
+          let [personId, name] = arr;
+          let ans = " ID = " + personId + " Name = "+ name
+          document.getElementById("desstruc").innerHTML = ans
+//es6 method shorthand in objects
+          var myObject = {
+            userFirstName: 'Pinki',
+            userLastName: 'Thakor',
+            output() {
+              let printy = document.getElementById('printShortObj')
+
+              printy.innerHTML = this.userFirstName + ' ' + this.userLastName;
+            }
+          };
+         
+          myObject.output();
+
+
+
      }
+  
